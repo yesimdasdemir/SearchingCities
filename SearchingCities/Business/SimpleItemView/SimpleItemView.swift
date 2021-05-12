6def706ff5ff2b085f52bfae5c7ce9555b39b7c6
Yeşim Daşdemir <yesimdasdemiriu@gmail.com>
2021-05-13 01:59:27 +0300

    [DEV][yesim.dasdemir][City list and detail pages were created.]

---
+++ SearchingCities/Business/SimpleItemView/SimpleItemView.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleItemView.swift
+//  SearchingCities
+//
+//  Created by Yeşim Daşdemir on 13.05.2021.
+//
+
+import UIKit
+
+class SimpleItemView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- SearchingCities/ViewController.swift
+++ SearchingCities/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController, UISearchResultsUpdating, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    var items: [String] = []
+    
+    private let searchController = UISearchController(searchResultsController: nil)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        registerTableView()
+        setSearchViewController()
     }
 
+    private func registerTableView() {
+        
+    }
+    
+    private func setSearchViewController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Cities"
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.accessibilityScroll(.down)
+        definesPresentationContext = true
+    }
 
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
