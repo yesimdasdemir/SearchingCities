630efe6596b0d3a16798aa5290b158e9ff774756
Yeşim Daşdemir <yesimdasdemiriu@gmail.com>
2021-05-15 16:04:29 +0300

    [DEV][yesim.dasdemir][Unit test classes were created.]

---
+++ SearchingCitiesTests/SearchingCitiesTests.swift
@@ -0,0 +1,32 @@
+//
+//  SearchingCitiesTests.swift
+//  SearchingCitiesTests
+//
+//  Created by Yeşim Daşdemir on 15.05.2021.
+//
+
+import XCTest
+
+class SearchingCitiesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
