20515ccd5f1246d476c74a5f0e7232b7dad847ff
Yeşim Daşdemir <yesimdasdemiriu@gmail.com>
2021-05-17 15:54:45 +0300

    [FIX][yesim.dasdemir][Error fixed.]

--- SearchingCities/AppDelegate.swift
+++ SearchingCities/AppDelegate.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 @available(iOS 13.0, *)
-@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
